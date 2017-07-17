47065cbba5c415f5b8653334d4a29ee3509d67cc
pacificsoftdev <pacificsoftdev.hd@gmail.com>
2017-07-17 08:51:26 +0700

    change database form mysql to sqli

--- neryt/settings.py
+++ neryt/settings.py
@@ -109,16 +109,16 @@ WSGI_APPLICATION = 'neryt.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    "default": {
-        "NAME": "neryt",
-        "ENGINE": "django.db.backends.mysql",
-        "USER": "root",
-        "PASSWORD": "Lumilarg_0",
-    },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+    # "default": {
+    #     "NAME": "neryt",
+    #     "ENGINE": "django.db.backends.mysql",
+    #     "USER": "root",
+    #     "PASSWORD": "Lumilarg_0",
+    # },
 }
 
 
